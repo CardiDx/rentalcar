f9f9da320474a44f78cfd5e5650f526431e0b1af
Sonale17! <verbitskiya@bk.ru>
2022-08-14 16:13:02 +0300

    fixed bugs home page

--- js/app.js
+++ js/app.js
@@ -1,57 +1,55 @@
-// const header = document.querySelector("[data-js-header]");
-// const headerScrolledClass =
-//   header.getAttribute("data-js-header-scrolled") || "";
-// const headerOpenedClass = header.getAttribute("data-js-header-opened") || "";
-// const headerToggle = document.querySelector("[data-js-header-toggle]");
-// const headerToggleActiveClass =
-//   headerToggle.getAttribute("data-js-header-toggle-active") || "";
+const header = document.querySelector("[data-js-header]");
+const headerScrolledClass =
+  header.getAttribute("data-js-header-scrolled") || "";
+const headerOpenedClass = header.getAttribute("data-js-header-opened") || "";
+const headerToggle = document.querySelector("[data-js-header-toggle]");
+const headerToggleActiveClass =
+  headerToggle.getAttribute("data-js-header-toggle-active") || "";
 
-// function animationHeader(item, currentPosition) {
-//   if (!item) {
-//     return;
-//   }
-//   currentPosition > 96
-//     ? item.classList.add(headerScrolledClass)
-//     : item.classList.remove(headerScrolledClass);
-// }
+function animationHeader(item, currentPosition) {
+  if (!item) {
+    return;
+  }
+  currentPosition > 96
+    ? item.classList.add(headerScrolledClass)
+    : item.classList.remove(headerScrolledClass);
+}
 
-// animationHeader(
-//   header,
-//   window.pageYOffset || document.documentElement.scrollTop
-// );
+animationHeader(
+  header,
+  window.pageYOffset || document.documentElement.scrollTop
+);
 
-// window.addEventListener("scroll", function () {
-//   const scrolled = window.pageYOffset || document.documentElement.scrollTop;
-//   animationHeader(header, scrolled);
-// });
+window.addEventListener("scroll", function () {
+  const scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  animationHeader(header, scrolled);
+});
 
-// headerToggle &&
-//   header &&
-//   (headerToggle.onclick = (e) => {
-//     header.classList.toggle(headerOpenedClass);
-//     e.target.classList.toggle(headerToggleActiveClass);
-//   });
+headerToggle &&
+  header &&
+  (headerToggle.onclick = (e) => {
+    header.classList.toggle(headerOpenedClass);
+    e.target.classList.toggle(headerToggleActiveClass);
+  });
 
-// var headerLinks = document.querySelectorAll(".c-header__menu-link");
-// headerLinks.forEach(function (headerLink) {
-//   headerLink.addEventListener("click", function () {
-//     header.classList.remove("c-header--opened");
-//     headerToggle.classList.remove("c-header__burger--active");
-//   });
-// });
+var headerLinks = document.querySelectorAll(".c-header__menu-link");
+headerLinks.forEach(function (headerLink) {
+  headerLink.addEventListener("click", function () {
+    header.classList.remove("c-header--opened");
+    headerToggle.classList.remove("c-header__burger--active");
+  });
+});
 
 let checkboxSearch = document.getElementById("check1");
 const addInput = document.querySelector(".c-search-panel__field--hidden");
 
-checkboxSearch.addEventListener('change', function() {
+checkboxSearch.addEventListener("change", function () {
   if (this.checked) {
     addInput.style.display = "block";
   } else {
     addInput.style.display = "none";
   }
 });
-
-
 
 // // popups
 
